752e123bb1e0ce51f71aec5b0a2a4fa69dc5c581
Piotr Kowalik <kowalikk.piotr@gmail.com>
2019-11-20 20:57:28 +0100

    lab7ex4

--- src/EmployeeForm.jsx
+++ src/EmployeeForm.jsx
@@ -10,45 +10,50 @@ class EmployeeForm extends React.Component {
             email: "",
             parentName: "",
             parentPhoneNumber: "",
-            errors: {
-                isParentPhoneNumberValid: true,
-                isEmailValid: true
-            }
+            isFormValid: false,
+            isParentPhoneNumberValid: true,
+            isEmailValid: true
         }
     }
-    phoneNumberValidation = () => {
-        return this.state.parentPhoneNumber.length === 9 && this.state.parentPhoneNumber.match("[0-9]{9}").length === 1;
-    }
-    
-    emailValidation = () => {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(this.state.email).toLowerCase());
+    phoneNumberValidation = (phoneNumber) => {
+        return phoneNumber.length === 9 && phoneNumber.match("[0-9]{9}").length === 1;
     }
 
+    emailValidation = (email) => {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
 
-    createNewEmoplyee = (e) => {
-        e.preventDefault();
-        let v = this.state.errors;
-
-        if (this.state.age >= 18)
-            v.isEmailValid = this.emailValidation();
-        else
-            v.isParentPhoneNumberValid = this.phoneNumberValidation();
-
+    onEmailChange = (e) => {
         this.setState({
-            errors: v
+            email: e.target.value,
+            isEmailValid: this.emailValidation(e.target.value)
         });
     }
 
+    onPhoneNumberChange = (e) => {
+        this.setState({
+            email: e.target.value,
+            isParentPhoneNumberValid: this.phoneNumberValidation(e.target.value)
+        });
+    }
+    validate = () => {
+        this.setState(prevState => ({
+            isEmailValid: this.emailValidation(prevState.email),
+            isParentPhoneNumberValid: this.phoneNumberValidation(prevState.parentPhoneNumber)
+        }));
+    }
+
+    createNewEmployee = (e) => {
+        e.preventDefault();
+        this.validate();
+    }
+
     render() {
-        if (this.state.loading) {
-            return (
-                <label>Saving...</label>
-            )
-        }
+
         return (
             <div>
-                <form onSubmit={this.createNewEmoplyee}>
+                <form onSubmit={this.createNewEmployee}>
                     <div className="row">
                         <label>Age</label>
                         <input style={{ margin: "10px" }} defaultValue={this.state.age} type="number" onChange={(e) => { this.setState({ age: e.target.value }) }} />
@@ -57,12 +62,12 @@ class EmployeeForm extends React.Component {
                         <div>
                             <div className="row">
                                 <label>Parent Name</label>
-                                <input style={{ margin: "10px" }} onChange={(e) => { this.setState({ parentName: e.target.value }) }} />
+                                <input style={{ margin: "10px" }} onChange={(e) => { this.setState({ parentName: e.target.value }); }} />
                             </div>
                             <div className="row">
                                 <label>Parent Phone No.</label>
-                                <input style={{ margin: "10px" }} onChange={(e) => { this.setState({ parentPhoneNumber: e.target.value }) }} />
-                                <span>{this.state.errors.isParentPhoneNumberValid ? "" : "Parent phone number is not valid!"}</span>
+                                <input style={{ margin: "10px" }} onChange={this.onEmailChange} />
+                                <span>{this.state.isParentPhoneNumberValid ? "" : "Parent phone number is not valid!"}</span>
                             </div>
                         </div>
                     }
@@ -70,19 +75,20 @@ class EmployeeForm extends React.Component {
                         <div>
                             <div className="row">
                                 <label>Name</label>
-                                <input style={{ margin: "10px" }} onChange={(e) => { this.setState({ name: e.target.value }) }} />
+                                <input style={{ margin: "10px" }} onChange={(e) => { this.setState({ name: e.target.value }); }} />
                             </div>
                             <div className="row">
                                 <label>Email</label>
-                                <input style={{ margin: "10px" }} onChange={(e) => { this.setState({ email: e.target.value }) }} />
-                                <span>{this.state.errors.isEmailValid ? "" : "Email is not valid!"}</span>
+                                <input style={{ margin: "10px" }} onChange={this.onEmailChange} />
+                                <span>{this.state.isEmailValid ? "" : "Email is not valid!"}</span>
                             </div>
                         </div>
                     }
-                    <button type="submit" >Submit</button>
+                    <button type="submit" disabled={!(this.state.isEmailValid && this.state.age >= 18 || this.state.age < 18 && this.state.isParentPhoneNumberValid)} >Submit</button>
                 </form>
             </div>
         );
+
     }
 }
 
